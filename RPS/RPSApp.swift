254ab4ea15784970a81789857e9c65a6624a9f88
Jason Dank <116655607+jdank417@users.noreply.github.com>
2026-08-25 06:27:59 -0400

    Initial Commit

---
+++ RPS/RPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSApp.swift
+//  RPS
+//
+//  Created by Jason Dank on 8/25/26.
+//
+
+import SwiftUI
+
+@main
+struct RPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RPSTests/RPSTests.swift
@@ -0,0 +1,17 @@
+//
+//  RPSTests.swift
+//  RPSTests
+//
+//  Created by Jason Dank on 8/25/26.
+//
+
+import Testing
+@testable import RPS
+
+struct RPSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
